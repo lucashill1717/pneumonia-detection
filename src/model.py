c3fd9a2b266dd5cf83f46ae29109b558253387b5
lucashill1717 <lucashill1717@gmail.com>
2024-04-25 16:39:26 -0600

    experimental model is set up

--- src/model.py
+++ src/model.py
@@ -1,11 +1,30 @@
-from keras.preprocessing import image_dataset_from_directory as idfd
-from keras import Sequential
+from keras.preprocessing import image_dataset_from_directory
+from keras.models import Sequential
+from keras.layers import Input, Conv2D, MaxPooling2D, Dense
 
-training_data = idfd(
-    "data/train",
+training_data = image_dataset_from_directory(
+    directory="data/train",
     image_size=(512, 512),
     color_mode="grayscale",
     interpolation="gaussian",
+    label_mode="categorical"
 )
 
+model = Sequential(layers=(
+    Input(shape=(512, 512, 1)),
+    Conv2D(64, 3, activation="relu"),
+    Conv2D(64, 3, activation="relu"),
+    MaxPooling2D(),
+    Conv2D(128, 3, activation="silu"),
+    Conv2D(128, 3, activation="silu"),
+    MaxPooling2D(),
+    Dense(3, activation="softmax")
+))
 
+model.compile(
+    optimizer="adam",
+    loss="categoricalcrossentropy",
+    metrics=(
+        
+    )
+)
