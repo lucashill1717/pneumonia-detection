01475d9e5f0eb2eb085b8a0158b7c568428e3f9f
lucashill1717 <lucashill1717@gmail.com>
2024-04-25 00:16:17 -0600

    starting the model process

--- src/model.py
+++ src/model.py
@@ -1,3 +1,11 @@
-import keras
+from keras.preprocessing import image_dataset_from_directory as idfd
+from keras import Sequential
 
-print(keras.__version__)
+training_data = idfd(
+    "data/train",
+    image_size=(512, 512),
+    color_mode="grayscale",
+    interpolation="gaussian",
+)
+
+
