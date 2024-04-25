56d26db55f889584392e4cd57ed45af60cd1d368
lucashill1717 <lucashill1717@gmail.com>
2024-04-25 17:16:08 -0600

    training works

--- src/model.py
+++ src/model.py
@@ -1,30 +1,39 @@
+import tensorflow as tf
 from keras.preprocessing import image_dataset_from_directory
 from keras.models import Sequential
-from keras.layers import Input, Conv2D, MaxPooling2D, Dense
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+
+DIMENSION = 256
 
 training_data = image_dataset_from_directory(
     directory="data/train",
-    image_size=(512, 512),
+    image_size=(DIMENSION, DIMENSION),
     color_mode="grayscale",
     interpolation="gaussian",
-    label_mode="categorical"
+    label_mode="categorical",
 )
 
-model = Sequential(layers=(
-    Input(shape=(512, 512, 1)),
-    Conv2D(64, 3, activation="relu"),
-    Conv2D(64, 3, activation="relu"),
-    MaxPooling2D(),
-    Conv2D(128, 3, activation="silu"),
-    Conv2D(128, 3, activation="silu"),
-    MaxPooling2D(),
-    Dense(3, activation="softmax")
-))
+model = Sequential(
+    layers=(
+        Input(shape=(DIMENSION, DIMENSION, 1)),
+        Conv2D(64, 3, activation="relu"),
+        Conv2D(64, 3, activation="relu"),
+        MaxPooling2D(),
+        Conv2D(128, 3, activation="silu"),
+        Conv2D(128, 3, activation="silu"),
+        MaxPooling2D(),
+        Flatten(),
+        Dense(3, activation="softmax"),
+    )
+)
 
 model.compile(
-    optimizer="adam",
-    loss="categoricalcrossentropy",
-    metrics=(
-        
-    )
+    optimizer="adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"]
+)
+
+model.fit(
+    x=training_data,
+    batch_size=32,
+    epochs=20,
+    verbose=2,
 )
