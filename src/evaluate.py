d453f7b815bceee2d67d05a9b4daec68b5710b61
lucashill1717 <lucashill1717@gmail.com>
2024-04-29 14:09:20 -0600

    decided a generator will be needed for my purposes, starting that process

--- src/evaluate.py
+++ src/evaluate.py
@@ -5,11 +5,11 @@ model = load_model("model0.keras")
 
 validation_data = image_dataset_from_directory(
     directory="data/val",
-    image_size=(256, 256),
+    image_size=(320, 320),
     color_mode="grayscale",
     interpolation="gaussian",
     label_mode="categorical",
     verbose=False,
 )
 
-model.evaluate(validation_data) # type: ignore
+model.evaluate(validation_data)  # type: ignore
--- src/model.py
+++ src/model.py
@@ -1,10 +1,12 @@
 from keras.preprocessing import image_dataset_from_directory
 from keras.models import Sequential
 from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.utils import PyDataset
+
 
 training_data = image_dataset_from_directory(
     directory="data/train",
-    image_size=(384, 384),
+    image_size=(320, 320),
     color_mode="grayscale",
     interpolation="gaussian",
     label_mode="categorical",
@@ -13,15 +15,14 @@ training_data = image_dataset_from_directory(
 
 model = Sequential(
     layers=[
-        Input(shape=(384, 384, 1)),
-        Conv2D(128, 5, activation="relu"),
+        Input(shape=(320, 320, 1)),
+        Conv2D(64, 3, activation="relu"),
+        Conv2D(64, 3, activation="relu"),
+        MaxPooling2D(pool_size=(3, 3)),
+        Conv2D(128, 3, activation="relu"),
         Conv2D(128, 3, activation="relu"),
         Dropout(rate=0.1),
-        MaxPooling2D(),
-        Conv2D(256, 5, activation="relu"),
-        Conv2D(256, 3, activation="relu"),
-        Dropout(rate=0.1),
-        MaxPooling2D(),
+        MaxPooling2D(pool_size=(3, 3)),
         Flatten(),
         Dense(3, activation="softmax"),
     ]
@@ -31,9 +32,6 @@ model.compile(
     optimizer="adam", loss="categorical_crossentropy", metrics=["categorical_accuracy"]
 )
 
-model.fit(
-    x=training_data,
-    epochs=100,
-)
+model.fit(x=training_data, epochs=100)
 
 model.save("model0.keras")
