c5cc16b32e63eae36f4976c1561a5fec2b6e7999
lucashill1717 <lucashill1717@gmail.com>
2024-04-30 15:56:50 -0600

    typing and black changes

--- src/dataset.py
+++ src/dataset.py
@@ -1,32 +1,37 @@
+from typing import Any
 from keras.utils import PyDataset
 from math import ceil
 from skimage.io import imread
 from skimage.transform import resize
-import numpy as np
+from numpy import array, ndarray
+
 
 def _get_image_paths(dir: str) -> tuple[list[str], list[list]]:
     paths, classes = [], []
     return paths, classes
 
+
 class PneumoniaDataset(PyDataset):
     """
-    Dataset specifically tailored to the Pneumonia Dataset.
-    Utilizes a Keras PyDataset class, allowing a generator
-    to be used to avoid memory issues.
+    Dataset class specifically tailored to the Pneumonia Detection dataset from Kaggle.
+    Utilizes a Keras PyDataset class, allowing a generator to be used to avoid memory issues.
     """
-    def __init__(self, dir: str, batch_size: int, **kwargs):
+
+    def __init__(
+        self: "PneumoniaDataset", dir: str, batch_size: int, **kwargs: Any
+    ) -> None:
         super().__init__(**kwargs)
         self.x, self.y = _get_image_paths(dir)
         self.batch_size = batch_size
 
-    def __len__(self) -> int:
+    def __len__(self: "PneumoniaDataset") -> int:
         return ceil(len(self.x) / self.batch_size)
 
-    def __getitem__(self, idx: int):
+    def __getitem__(self: "PneumoniaDataset", idx: int) -> tuple[ndarray, ndarray]:
         low = idx * self.batch_size
         high = min(low + self.batch_size, len(self.x))
         batch_x = self.x[low:high]
         batch_y = self.y[low:high]
-        return np.array(
+        return array(
             [resize(imread(file_name), (1416, 1736)) for file_name in batch_x]
-        ), np.array(batch_y)
+        ), array(batch_y)
