b711710b4353a2aacd72a33d3a2c5c44f9a27f72
lucashill1717 <lucashill1717@gmail.com>
2024-05-08 19:24:44 -0600

    trying a new model with normalization and different activations

--- src/model.py
+++ src/model.py
@@ -1,6 +1,6 @@
 from dataset import PneumoniaDataset, HEIGHT, WIDTH
 from keras.models import Sequential
-from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, Activation
 
 
 training_data = PneumoniaDataset("data/train", 8)
@@ -8,19 +8,25 @@ training_data = PneumoniaDataset("data/train", 8)
 model = Sequential(
     layers=[
         Input(shape=(HEIGHT, WIDTH, 1)),
-        Conv2D(32, 3, activation="relu"),
-        Conv2D(32, 3, activation="relu"),
+        Conv2D(32, 3),
+        BatchNormalization(),
+        Activation("elu"),
+        Conv2D(32, 3),
+        BatchNormalization(),
+        Activation("relu"),
         MaxPooling2D(2),
-        Conv2D(64, 3, activation="relu"),
-        Conv2D(64, 3, activation="relu"),
+        Conv2D(64, 3),
+        BatchNormalization(),
+        Activation("elu"),
+        Conv2D(64, 3),
+        BatchNormalization(),
+        Activation("relu"),
+        MaxPooling2D(3),
+        Conv2D(128, 3),
+        BatchNormalization(),
+        Activation("relu"),
         Dropout(0.1),
-        MaxPooling2D(2),
-        Conv2D(128, 3, activation="relu"),
-        Conv2D(128, 3, activation="relu"),
-        MaxPooling2D(2),
         Flatten(),
-        Dense(32, activation="relu"),
-        Dropout(0.2),
         Dense(3, activation="softmax"),
     ]
 )
